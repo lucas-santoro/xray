e67d458c7102d42b2e8257c8ae24495a1c51932f
Thomas Marchand <me@thomas-marchand.com>
2021-08-07 14:26:07 +0200

    initial commit

---
+++ src/main/java/io/th0rgal/xray/XrayPlugin.java
@@ -0,0 +1,17 @@
+package io.th0rgal.xray;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class XrayPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
