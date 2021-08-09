25f558201cd9987ad8d82edf0b9056924dbcf4ad
Thomas Marchand <me@thomas-marchand.com>
2021-08-09 15:18:38 +0200

    update inventories

--- src/main/java/io/th0rgal/xray/menus/CategoryMenu.java
+++ src/main/java/io/th0rgal/xray/menus/CategoryMenu.java
@@ -2,6 +2,7 @@ package io.th0rgal.xray.menus;
 
 import com.github.stefvanschie.inventoryframework.gui.GuiItem;
 import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
+import com.github.stefvanschie.inventoryframework.pane.PaginatedPane;
 import com.github.stefvanschie.inventoryframework.pane.StaticPane;
 import io.th0rgal.xray.XrayPlugin;
 import io.th0rgal.xray.config.Config;
@@ -15,6 +16,8 @@ import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class CategoryMenu {
@@ -22,19 +25,20 @@ public class CategoryMenu {
     private ChestGui mainGui;
 
     public ChestGui create() {
-        int rows = 2;
-        mainGui = new ChestGui(rows, "XRay");
+        mainGui = new ChestGui(6, "XRay");
         ConfigurationSection categories = Config.MENU_CATEGORIES.toConfigSection();
 
-        final StaticPane pane = new StaticPane(0, 0, 9, rows);
+        final StaticPane pane = new StaticPane(0, 0, 9, 6);
         int i = 0;
         for (String name : categories.getKeys(false).stream().sorted().collect(Collectors.toList())) {
             pane.addItem(new GuiItem(getItemStack(Config.MENU_CATEGORY_ITEM.toItem(name),
                     Config.MENU_CATEGORY_NAME.toSerializedString(name)),
-                    event -> getCategoryGUI(event.getWhoClicked(), name).show(event.getWhoClicked())), i++, 0);
+                            event -> getCategoryGUI(event.getWhoClicked(), name).show(event.getWhoClicked())),
+                    i % 9, i / 9);
+            i++;
         }
         pane.addItem(new GuiItem(getItemStack(Material.BARRIER, Config.EXIT.toSerializedString()),
-                event -> event.getWhoClicked().closeInventory()), 4, 1);
+                event -> event.getWhoClicked().closeInventory()), 4, 6);
 
         mainGui.addPane(pane);
         return mainGui;
@@ -42,9 +46,10 @@ public class CategoryMenu {
 
     private ChestGui getCategoryGUI(HumanEntity owner, String name) {
         final DisplayData data = XrayPlugin.get().getDisplayData(owner);
-        ChestGui gui = new ChestGui(4, "XRay - " + name);
-        final StaticPane pane = new StaticPane(0, 0, 9, 4);
-        int i = 0;
+        ChestGui gui = new ChestGui(6, "XRay - " + name);
+        final PaginatedPane pane = new PaginatedPane(9, 5);
+
+        List<GuiItem> items = new ArrayList<>();
         for (String block : Config.MENU_CATEGORY_BLOCKS.toConfigSection(name).getKeys(false)) {
             ItemStack stack = Config.MENU_CATEGORY_BLOCKS_TYPE.toItem(name, block);
             ItemMeta meta = stack.getItemMeta();
@@ -53,7 +58,7 @@ public class CategoryMenu {
             if (data.getColor(stack.getType()) != 0)
                 meta.addEnchant(Enchantment.DURABILITY, 1, true);
             stack.setItemMeta(meta);
-            pane.addItem(new GuiItem(stack, event -> {
+            items.add(new GuiItem(stack, event -> {
                 updateDisplayData(stack.getType(),
                         Config.MENU_CATEGORY_BLOCKS_COLOR.toColor(name, block),
                         event.getWhoClicked());
@@ -63,15 +68,57 @@ public class CategoryMenu {
                     meta.removeEnchant(Enchantment.DURABILITY);
                 stack.setItemMeta(meta);
                 gui.update();
-            }), i++, 0);
+            }));
         }
 
-        pane.addItem(new GuiItem(getItemStack(Material.BARRIER, Config.BACK.toSerializedString()),
-                (event) -> mainGui.show(event.getWhoClicked())), 4, 3);
+        for (int i = 0; i < (items.size() - 1) / 45 + 1; i++) {
+            final List<GuiItem> itemStackList = extractPageItems(items, i);
+            final StaticPane staticPane = new StaticPane(9, Math.min((itemStackList.size() - 1) / 9 + 1, 5));
+            for (int itemIndex = 0; itemIndex < itemStackList.size(); itemIndex++)
+                staticPane.addItem(itemStackList.get(itemIndex), itemIndex % 9, itemIndex / 9);
+            pane.addPane(i, staticPane);
+        }
+
+        //page selection
+        final StaticPane back = new StaticPane(2, 5, 1, 1);
+        final StaticPane forward = new StaticPane(6, 5, 1, 1);
+        final StaticPane exit = new StaticPane(4, 5, 1, 1);
+
+        back.addItem(new GuiItem(new ItemStack(Material.ARROW), event -> {
+            pane.setPage(pane.getPage() - 1);
+            if (pane.getPage() == 0) back.setVisible(false);
+            forward.setVisible(true);
+            gui.update();
+        }), 0, 0);
+        back.setVisible(false);
+
+        forward.addItem(new GuiItem(new ItemStack(Material.ARROW), event -> {
+            pane.setPage(pane.getPage() + 1);
+            if (pane.getPage() == pane.getPages() - 1) forward.setVisible(false);
+            back.setVisible(true);
+            gui.update();
+        }), 0, 0);
+
+        if (pane.getPages() <= 1)
+            forward.setVisible(false);
+
+        exit.addItem(new GuiItem(getItemStack(Material.BARRIER, Config.BACK.toSerializedString()),
+                (event) -> mainGui.show(event.getWhoClicked())), 0, 0);
+
         gui.setOnTopClick(event -> event.setCancelled(true));
+        gui.addPane(back);
+        gui.addPane(forward);
+        gui.addPane(exit);
         gui.addPane(pane);
         return gui;
     }
+
+    private List<GuiItem> extractPageItems(final List<GuiItem> items, final int page) {
+        final List<GuiItem> output = new ArrayList<>();
+        for (int i = page * 45; i < (page + 1) * 45 && i < items.size(); i++) output.add(items.get(i));
+        return output;
+    }
+
 
     private void updateDisplayData(Material type, int color, HumanEntity player) {
         DisplayData data = XrayPlugin.get().getDisplayData(player);
